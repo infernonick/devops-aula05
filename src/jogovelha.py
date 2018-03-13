3a42d4b7db802ab59871997049127cad9a787ee6
infernonick <36251495+infernonick@users.noreply.github.com>
2018-03-12 21:20:30 -0300

    Corrigida Identação Jogovelha.py

--- src/jogovelha.py
+++ src/jogovelha.py
@@ -1,10 +1,13 @@
-# Arquitetura
-* As funções relacionadas ao gerenciamento das casas do
-jogo da velha ficarão no módulo **jogovelha.py**.
-* O estado de cada casa do jogo será representada por uma
-string: "." para casa vazia; "X" para casa ocupada pelo 1o
-jogador; "O" para casa ocupada pelo 2o jogador
-* A função inicializar() retornará uma lista 3x3, onde cada
-posição conterá uma string para indicar o estado de uma
-casa do jogo. A função retornará todas as casas
-inicialmente vazias.
+def inicializar():
+    tab = []
+    for i in range(3):
+      linha = []
+      for j in range(3):
+        linha.append(".")
+      tab.append(linha)
+    return tab
+def main():
+  jogo = inicializar()
+  print(jogo)
+if __name__ == "__main__":
+  main()
